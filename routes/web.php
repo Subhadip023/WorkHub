ba8046a6703d7a528698cbb58e14df11d9cc201d
Ravi Shankar <ravishankar14012001@gmail.com>
2025-12-18 13:09:08 +0530

    comiite

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CompanyController;
-use App\Http\Controllers\TaskController;
 
-Route::get('/company/create', [CompanyController::class, 'create'])->name('company.create');
-Route::post('/company/store', [CompanyController::class, 'store'])->name('company.store');
-
-Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-Route::post('/tasks/store', [TaskController::class, 'store'])->name('tasks.store');
-
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/', function () {
     return view('welcome');
