c75f438276a106f29c90fc2b1efc4dbf9da31414
debashismidya <21026148+debashismidya@users.noreply.github.com>
2025-12-31 21:18:02 +0530

    dashboard and auth

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -81,7 +81,7 @@
                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                         Logout
                     </a>
-                    <form id="logout-form" action="#" method="POST" class="d-none">
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                         @csrf
                     </form>
                 </div>
