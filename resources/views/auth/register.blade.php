6aeaac0f8b0997a93ed674762bbaa84d571a61e2
debashismidya <debashis.matainja@gmail.com>
2026-05-30 11:02:05 +0530

    style : change defult register page to my own desing , now UI for both register and Login page same

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,83 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+@extends('layouts.auth')
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+@section('title', 'Register')
+
+@section('content')
+<div class="container" style="min-height: 100vh;">
+    <div class="row justify-content-center align-items-center" style="min-height: 100vh;">
+
+        <div class="col-xl-10 col-lg-12 col-md-9">
+
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg-6 d-none d-lg-block bg-register-image">
+                            <div class="card-body">
+                                <div class="text-center">
+                                    <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;"
+                                         src="{{ asset('asset/img/undraw_rocket.svg') }}" alt="...">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                                </div>
+                                <form class="user" method="POST" action="{{ route('register') }}">
+                                    @csrf
+
+                                    <div class="form-group">
+                                        <input type="text" name="name" class="form-control form-control-user"
+                                               id="exampleInputName" placeholder="Full Name" value="{{ old('name') }}" required autofocus autocomplete="name">
+                                    </div>
+                                    @error('name')
+                                        <span class="text-danger small">{{$message}}</span>
+                                    @enderror
+
+                                    <div class="form-group">
+                                        <input type="email" name="email" class="form-control form-control-user"
+                                               id="exampleInputEmail" placeholder="Email Address" value="{{ old('email') }}" required autocomplete="username">
+                                    </div>
+                                    @error('email')
+                                        <span class="text-danger small">{{$message}}</span>
+                                    @enderror
+
+                                    <div class="form-group">
+                                        <input type="password" name="password" class="form-control form-control-user"
+                                               id="exampleInputPassword" placeholder="Password" required autocomplete="new-password">
+                                    </div>
+                                    @error('password')
+                                        <span class="text-danger small">{{$message}}</span>
+                                    @enderror
+
+                                    <div class="form-group">
+                                        <input type="password" name="password_confirmation" class="form-control form-control-user"
+                                               id="exampleRepeatPassword" placeholder="Confirm Password" required autocomplete="new-password">
+                                    </div>
+                                    @error('password_confirmation')
+                                        <span class="text-danger small">{{$message}}</span>
+                                    @enderror
+
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                        Register Account
+                                    </button>
+                                    <hr>
+
+                                </form>
+                                <div class="text-center">
+                                    <a class="small" href="{{ route('login') }}">Already have an account? Login!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+    </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</div>
+@endsection
