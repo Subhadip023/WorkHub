bb6cc126d717bd3c0996e92feccd9b726cf4ae1c
debashismidya <98264381+Subhadip023@users.noreply.github.com>
2026-07-25 20:57:39 +0530

    style : change footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,13 +1,3 @@
-<footer class="sticky-footer bg-white">
-    <div class="container">
-        <div class="d-flex flex-column flex-md-row justify-content-between align-items-center text-center">
-            <div class=" mb-md-0">
-                {{ session('current_company_data')->name ?? '' }}
-            </div>
-
-            <div>
-                <span>Copyright &copy; WorkHub {{ date('Y') }}</span>
-            </div>
-        </div>
-    </div>
-</footer>
+<div class="sticky-footer">
+    <span>@ WorkHub {{ date('Y') }}</span>
+</div>
